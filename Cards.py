489e21ac1d94d8eda29decd4bf8770389c5f9ca2
Miniki <miniki2002@yandex.ua>
2022-08-02 12:47:40 +0500

    Закончены пак белых карт и начат пак красных

--- Cards.py
+++ Cards.py
@@ -1,12 +1,17 @@
-"""
-with open("./red_cards.txt", encoding='utf-8') as RCards_txt:
+import re
+
+with open("./src/red_cards.txt", encoding='utf-8-sig') as RCards_txt:
     RCards_list = RCards_txt.read()
-"""
-with open(r"C:\Users\Михаил\Documents\GitHub\500EnilCards\white_cards.txt", encoding='utf-8-sig') as WCards_txt:
+
+with open("./src/white_cards.txt", encoding='utf-8-sig') as WCards_txt:
     WCards_list = WCards_txt.read()
-print(WCards_list)
+print(RCards_list)
 class WhiteCards:
     # создаем атрибуты класса
     WC_count = WCards_list.count(";") +  1  #  определяем количество карт
     WC_list = WCards_list.split(" ; ")  #  Формируем список из карт
 
+class RedCards:
+    # создаем атрибуты класса
+    RC_count = RCards_list.count(";") +  1  #  определяем количество карт
+    RC_list = RCards_list.split(" ; ")  #  Формируем список из карт
