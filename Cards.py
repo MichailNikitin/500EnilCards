ba89fa0808deddc160ecb6cce4c6b3292cc7fa77
Miniki <miniki2002@yandex.ua>
2022-08-05 09:35:25 +0500

    add red card

--- Cards.py
+++ Cards.py
@@ -1,17 +1,19 @@
 import re
+import os
+cwd = os.getcwd()  # Get the current working directory (cwd)
 
-with open("./src/red_cards.txt", encoding='utf-8-sig') as RCards_txt:
+with open(r"C:\Users\Михаил\Documents\GitHub\500EnilCards\src\red_cards.txt", encoding='utf-8-sig') as RCards_txt:
     RCards_list = RCards_txt.read()
 
-with open("./src/white_cards.txt", encoding='utf-8-sig') as WCards_txt:
+with open(r"C:\Users\Михаил\Documents\GitHub\500EnilCards\src\white_cards.txt", encoding='utf-8-sig') as WCards_txt:
     WCards_list = WCards_txt.read()
-print(RCards_list)
+
 class WhiteCards:
     # создаем атрибуты класса
     WC_count = WCards_list.count(";") +  1  #  определяем количество карт
-    WC_list = WCards_list.split(" ; ")  #  Формируем список из карт
+    WC_list = WCards_list.split("; ")  #  Формируем список из карт
 
 class RedCards:
     # создаем атрибуты класса
     RC_count = RCards_list.count(";") +  1  #  определяем количество карт
-    RC_list = RCards_list.split(" ; ")  #  Формируем список из карт
+    RC_list = re.sub("[_]+", "*****", RCards_list).split("; ")  #  Формируем список из карт
